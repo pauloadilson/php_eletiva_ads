ebfdf94b6748c6e2f60cfceeb95d4c4b0b780a35
Paulo Adilson da Silva <pauloadilson@gmail.com>
2020-09-08 20:50:42 -0300

    Organização dos exercícios da Lista 2

--- A04_lista2/index.php
+++ A04_lista2/index.php
@@ -6,29 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-    <title>Eletiva LP 4 - PHP - Listas de Exercícios</title>
-    <style>
-        .alt {
-            height: calc(1.5em + .75rem + 2px);
-        }
-    </style>
+    <title>Eletiva LP 4 - PHP - Lista de Exercícios 2</title>
 </head>
 
 <body>
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
-            <h1 class="display-4">LP 4 - PHP</h1>
+            <h1 class="display-4">Eletiva: LP 4 - PHP</h1>
             <p class="lead">Aulas e exercícios práticos da disciplina de ADS da FATEC de Presidente Prudente-SP.</p>
         </div>
         <div class="list-group">
-            <a href="#" class="list-group-item list-group-item-action active">Aulas e atividades</a>
-            <a href="A01/index.php" class="list-group-item list-group-item-action">Aula 01</a>
-            <a href="A02/metodos.html" class="list-group-item list-group-item-action">Aula 02 - Metodos Get Post</a>
-            <a href="A03_lista1/" class="list-group-item list-group-item-action">Aula 03 - Lista de Exercícios 1</a>
-            <a href="A04_lista2/" class="list-group-item list-group-item-action">Aula 04 - Funções</a>
-            <a href="" class="list-group-item list-group-item-action">Aula 05 -</a>
-            <a href="" class="list-group-item list-group-item-action">Aula 06 -</a>
-        </div>
+            <a href="#" class="list-group-item list-group-item-action active">Lista de Exercício 2</a>
+            <?php
+                $descricao = array('Menor de dois','Soma dos divisores','Verifica se é par ? 1 : 0','Positivo/Negativo/Zero','Calcula IMC','Cadastro e Verifica Senha','Busca na internet');
+                for ($i=1; $i<=7; $i++){
+                ?>
+                <a href="ex0<?= $i ?>.php" class="list-group-item list-group-item-action">Aula 0<?= $i ?> - <?= $descricao[$i-1] ?></a>
+            <?php
+                }   
+            ?>
+         </div>
     </div>
 
     <!-- Optional JavaScript -->
