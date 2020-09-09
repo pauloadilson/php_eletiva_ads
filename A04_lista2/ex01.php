ac3de38e6d9bbb0085eef33e85758b185e2c745d
Paulo Adilson da Silva <pauloadilson@gmail.com>
2020-09-08 21:30:40 -0300

    Exercício 1 implementado

---
+++ A04_lista2/ex01.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- jmask validação front end no JS -->
+    <title>Exercício 1 - Lista 2</title>
+    <style>
+    </style>
+</head>
+
+<body>
+    <div class="container p-4">
+        <h1>Exercício 1 - Lista 2</h1>
+        <div class=" p-4">
+            <form class="form" id="form1" method="post">
+                <div class="form-group row">
+                    <label for="numInt1" class="col-sm-8 col-form-label">Informe um número inteiro:</label>
+                    <div class="col-sm-4">
+                        <input type="number" class="form-control" id="numInt1" name="numInt1" placeholder='0' value="0" >
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="numInt2" class="col-sm-8 col-form-label">Informe outro número inteiro:</label>
+                    <div class="col-sm-4">
+                        <input type="number" class="form-control" id="numInt2" name="numInt2" placeholder='0' value="0" >
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <input type="submit" class="btn btn-primary" name="bt_ex01" value='Comparar e exibir'>
+                    </div>
+                </div>
+            </form>
+             
+                <?php 
+                function GetLower($num1, $num2) 
+                {
+                    if ($num1 < $num2)
+                        return $num1;
+                    else
+                        return $num2;
+                }
+
+                if($_POST) {
+                    $n1 = $_POST["numInt1"];
+                    $n2 = $_POST["numInt2"];
+                    $lower = GetLower($n1, $n2);
+                    $today = date("d/m/Y");
+                 echo "<div class='p-4 m-5 bg-dark text-white text-center'>
+                     Hoje é $today. <br>
+                    O menor número informado é $lower.
+                    </div>";
+                                    }
+                ?>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
